06b99f54ac1400b88974f2f665e2d0e2c431f6f8
“Ricardo1435” <ricantog1435@gmail.com>
2023-05-14 20:17:15 -0600

    Se crea sistema para proyecto de IA

---
+++ app/Models/Comentario.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comentario extends Model
+{
+    use HasFactory;
+    public $timestamps = true;
+    protected $table = "comentario";
+    protected $guarded = ['id'];
+    protected $fillable = ['nombre_cliente', 'telefono', 'correo', 'comentario'];
+}
